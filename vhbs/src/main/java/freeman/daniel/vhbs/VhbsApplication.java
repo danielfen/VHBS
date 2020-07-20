2f3e29f859b095dd22b67d57061929a63806973f
Daniel Freeman <Daniel.Freeman@scisys.co.uk>
2020-07-20 16:31:05 +0100

    Initial project commit : generated using sprint initializr

---
+++ vhbs/src/main/java/freeman/daniel/vhbs/VhbsApplication.java
@@ -0,0 +1,13 @@
+package freeman.daniel.vhbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhbsApplication.class, args);
+	}
+
+}
---
+++ vhbs/src/test/java/freeman/daniel/vhbs/VhbsApplicationTests.java
@@ -0,0 +1,13 @@
+package freeman.daniel.vhbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
